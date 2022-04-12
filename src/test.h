df951bf97978018d0b9de1aea223d3cc29173672
Rehaan Bhimani <bhimar@cs.washington.edu>
2022-04-12 12:49:58 -0700

    Add passed output in test suite, fix test_hwX declarations, verify HW2 for release

--- src/test.h
+++ src/test.h
@@ -6,9 +6,8 @@ extern int tests_total;
 extern int tests_fail;
 #define TEST(EX) do { ++tests_total; if(!(EX)) {\
     fprintf(stderr, "failed: [%s] testing [%s] in %s, line %d\n", __FUNCTION__, #EX, __FILE__, __LINE__); \
-    ++tests_fail; }} while (0)
+    ++tests_fail; }else{fprintf(stderr, "passed: [%s] testing [%s] in %s, line %d\n", __FUNCTION__, #EX, __FILE__, __LINE__);}} while (0)
 
-void test_hw0();
 void test_hw1();
 void test_hw2();
 void test_hw3();
